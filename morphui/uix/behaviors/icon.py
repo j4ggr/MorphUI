2845667192ac944fa20c9027dfb5bb0fa1e1b323
ch0rjg <reto.jaeggli@festo.net>
2025-10-31 10:48:21 +0100

    IconBehavior - added, normal and active icon properties - added, _update_icon method

--- morphui/uix/behaviors/icon.py
+++ morphui/uix/behaviors/icon.py
@@ -48,6 +48,32 @@ class MorphIconBehavior(MorphAppReferenceBehavior):
     and defaults to ''.
     """
 
+    active_icon = StringProperty('checkbox-marked')
+    """Icon name for the 'active' state of the widget.
+
+    The icon is displayed when the widget is in the 'active' state
+    (i.e., checked). The icon name should correspond to a valid icon in
+    the Material Design Icons library. To automatically switch icons
+    based on the `active` property, bind the :meth:`_update_icon` method
+    to the `active` property of the widget.
+
+    :attr:`active_icon` is a :class:`~kivy.properties.StringProperty` 
+    and defaults to `"checkbox-marked"`.
+    """
+
+    normal_icon = StringProperty('checkbox-blank-outline')
+    """Icon name for the 'normal' state of the widget.
+
+    The icon is displayed when the widget is in the 'normal' state
+    (i.e., unchecked). The icon name should correspond to a valid icon
+    in the Material Design Icons library. To automatically switch icons
+    based on the `active` property, bind the :meth:`_update_icon` method
+    to the `active` property of the widget.
+
+    :attr:`normal_icon` is a :class:`~kivy.properties.StringProperty` and
+    defaults to `"checkbox-blank-outline"`.
+    """
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         self.bind(icon=self._apply_icon) # type: ignore
@@ -73,3 +99,15 @@ class MorphIconBehavior(MorphAppReferenceBehavior):
         
         if hasattr(self, 'text') and hasattr(self, 'typography'):
             self.text = self.typography.get_icon_character(icon)
+    
+    def _update_icon(self, *args) -> None:
+        """Update the displayed icon based on the `active` state.
+        
+        This method switches the icon between `active_icon` and
+        `normal_icon` depending on whether the widget is active.
+        
+        Bind this method to the `active` property to automatically
+        update the icon when the state changes.
+        """
+        active = getattr(self, 'active', False)
+        self.icon = self.active_icon if active else self.normal_icon
