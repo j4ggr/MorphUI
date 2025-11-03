d8bf68d310d37471e4eaffcde3ac8ad8a373776b
ch0rjg <reto.jaeggli@festo.net>
2025-11-03 18:15:36 +0100

    menu - added, basic inheritance

--- morphui/uix/boxlayout.py
+++ morphui/uix/boxlayout.py
@@ -1,9 +1,9 @@
 from kivy.uix.boxlayout import BoxLayout
 
-from morphui.uix.behaviors import MorphColorThemeBehavior
-from morphui.uix.behaviors import MorphSurfaceLayerBehavior
 from morphui.uix.behaviors import MorphAutoSizingBehavior
+from morphui.uix.behaviors import MorphColorThemeBehavior
 from morphui.uix.behaviors import MorphDeclarativeBehavior
+from morphui.uix.behaviors import MorphSurfaceLayerBehavior
 
 from morphui.utils import clean_config
 
--- morphui/uix/menu.py
+++ morphui/uix/menu.py
@@ -1,5 +1,20 @@
 
+from kivy.uix.boxlayout import BoxLayout
+
+from morphui.uix.behaviors import MorphElevationBehavior
+from morphui.uix.behaviors import MorphAutoSizingBehavior
+from morphui.uix.behaviors import MorphColorThemeBehavior
+from morphui.uix.behaviors import MorphDeclarativeBehavior
+from morphui.uix.behaviors import MorphSurfaceLayerBehavior
+from morphui.uix.behaviors import MorphDropdownBehavior
 
 
 
-class Menu
+class MorphMenu(
+        MorphDeclarativeBehavior,
+        MorphAutoSizingBehavior,
+        MorphColorThemeBehavior,
+        MorphSurfaceLayerBehavior,
+        MorphElevationBehavior,
+        BoxLayout):
+    pass
