05e96afce91786087dcb434a70908466664d324f
ch0rjg <reto.jaeggli@festo.net>
2025-11-04 17:14:21 +0100

    menu - added, default_config for MorphMenu - added, MenuRecycleBoxLayout class

--- morphui/uix/menu.py
+++ morphui/uix/menu.py
@@ -1,6 +1,10 @@
+from typing import Any
+from typing import Dict
 
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
 
+from morphui.utils import clean_config
 from morphui.uix.behaviors import MorphElevationBehavior
 from morphui.uix.behaviors import MorphAutoSizingBehavior
 from morphui.uix.behaviors import MorphColorThemeBehavior
@@ -9,6 +13,13 @@ from morphui.uix.behaviors import MorphSurfaceLayerBehavior
 from morphui.uix.behaviors import MorphDropdownBehavior
 
 
+class MenuRecycleBoxLayout(
+        RecycleBoxLayout):
+    """A RecycleBoxLayout specifically for use within the MorphMenu
+    widget to layout menu items in a vertical list.
+    """
+    pass
+
 
 class MorphMenu(
         MorphDeclarativeBehavior,
@@ -16,5 +27,18 @@ class MorphMenu(
         MorphColorThemeBehavior,
         MorphSurfaceLayerBehavior,
         MorphElevationBehavior,
-        BoxLayout):
-    pass
+        RecycleView,):
+    """A MorphUI Menu widget that displays a list of items in a dropdown
+    menu. Inherits from multiple behaviors to provide a rich set of 
+    features including elevation, color theming, and auto-sizing.
+    """
+
+    default_config: Dict[str, Any] = dict(
+        elevation=2,)
+
+    def __init__(self, **kwargs) -> None:
+        config = clean_config(self.default_config, kwargs)
+        super().__init__(
+            MenuRecycleBoxLayout(identity='layout'),
+            **config)
+        self.layout=self.identities.layout
