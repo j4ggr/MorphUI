bbb5776ad857aecbf8043e92dc89110dfd52a181
ch0rjg <reto.jaeggli@festo.net>
2025-12-09 15:58:25 +0100

    DataView - added, navigation.py - added, classes used for navigation

--- morphui/uix/menu.py
+++ morphui/uix/menu.py
@@ -8,13 +8,11 @@ from kivy.uix.recycleboxlayout import RecycleBoxLayout
 
 from morphui.utils import clean_config
 from morphui.uix.behaviors import MorphElevationBehavior
-from morphui.uix.behaviors import MorphHoverBehavior
 from morphui.uix.behaviors import MorphInteractionLayerBehavior
 from morphui.uix.behaviors import MorphAutoSizingBehavior
 from morphui.uix.behaviors import MorphColorThemeBehavior
 from morphui.uix.behaviors import MorphDeclarativeBehavior
 from morphui.uix.behaviors import MorphSurfaceLayerBehavior
-from morphui.uix.behaviors import MorphDropdownBehavior
 
 
 class MorphMenuItem( # TODO: create basic LeadingTextTrailingContainer and inherit from it. Use the implementation of Chip as reference.
