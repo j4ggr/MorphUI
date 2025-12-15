da7f1af6cf32176845c288a608cb7a83dd286566
ch0rjg <reto.jaeggli@festo.net>
2025-12-15 14:11:12 +0100

    chip - changed, improved docstring example

--- morphui/uix/chip.py
+++ morphui/uix/chip.py
@@ -82,18 +82,42 @@ class MorphChip(
     Example
     -------
     ````python
+    from kivy.clock import Clock
     from morphui.app import MorphApp
     from morphui.uix.chip import MorphChip
+    from morphui.uix.chip import MorphInputChip
+    from morphui.uix.chip import MorphFilterChip
     from morphui.uix.floatlayout import MorphFloatLayout
 
     class MyApp(MorphApp):
         def build(self) -> MorphFloatLayout:
+            self.theme_manager.seed_color = 'morphui_teal'
             self.theme_manager.switch_to_dark()
-            return MorphFloatLayout(
+            self.layout = MorphFloatLayout(
                 MorphChip(
-                    identity='my_widget',
-                    leading_icon='language-python',),
+                    identity='chip',
+                    leading_icon='language-python',
+                    trailing_icon='close',
+                    label_text='Python Chip',
+                    pos_hint={'center_x': 0.5, 'center_y': 0.6},),
+                MorphFilterChip(
+                    identity='filter',
+                    label_text='Filter Chip',
+                    pos_hint={'center_x': 0.5, 'center_y': 0.5}),
+                MorphInputChip(
+                    identity='input_chip',
+                    label_text='Input Chip',
+                    pos_hint={'center_x': 0.5, 'center_y': 0.4},),
                 normal_surface_color=self.theme_manager.surface_color,)
+            self.input_chip = self.layout.identities.input_chip
+            return self.layout
+        
+        def on_start(self) -> None:
+            Clock.schedule_interval(self.re_add_chip, 2)
+
+        def re_add_chip(self, dt: float) -> None:
+            if not self.input_chip.parent:
+                self.layout.add_widget(self.input_chip)
 
     if __name__ == '__main__':
         MyApp().run()
