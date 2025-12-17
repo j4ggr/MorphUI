037d5cbce4935a75db950653c75b4ac666695cca
ch0rjg <reto.jaeggli@festo.net>
2025-12-17 14:15:25 +0100

    MorphMenuMotionBehavior - added, auto_adjust_position property to allow auto adjustement of menu or not - added, min_space_required to check minimum space required (in pixels) before adjusting menu position - remove, binding _adusting_and_reposition to size - changed, method name _update_position to _adusting_and_reposition - added, _adusting_and_reposition() method, that adjusts the menu anchor position and opening direction based on available space in the window. - added, _resolve_size method

--- morphui/uix/behaviors/motion.py
+++ morphui/uix/behaviors/motion.py
@@ -1,7 +1,10 @@
 from typing import Any
+from typing import Tuple
 from typing import Literal
 
+from kivy.metrics import dp
 from kivy.properties import AliasProperty
+from kivy.properties import BooleanProperty
 from kivy.properties import ObjectProperty
 from kivy.properties import StringProperty
 from kivy.properties import OptionProperty
@@ -125,21 +128,62 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
     :attr:`menu_window_margin` is a
     :class:`~kivy.properties.NumericProperty` and defaults to `8`."""
 
+    auto_adjust_position: bool = BooleanProperty(True)
+    """Whether to automatically adjust menu position to fit within window bounds.
+
+    This property determines if the menu should automatically adjust its
+    position and opening direction when there is insufficient space in
+    the window to fully display the menu. If set to `True`, the menu
+    will switch its anchor position and/or opening direction to fit
+    within the window bounds.
+
+    :attr:`auto_adjust_position` is a
+    :class:`~kivy.properties.BoolProperty` and defaults to `True`."""
+
+    min_space_required: float = NumericProperty(dp(100))
+    """Minimum space required (in pixels) before adjusting menu position.
+
+    This property defines the minimum amount of space (in pixels)
+    required in the desired direction for the menu to open. If there is
+    insufficient space, the menu may switch its position or opening
+    direction based on the :attr:`auto_adjust_position` setting.
+
+    :attr:`min_space_required` is a
+    :class:`~kivy.properties.NumericProperty` and defaults to `100`.
+    """
+
     def __init__(self, **kwargs) -> None:
         self.register_event_type('on_pre_open')
         self.register_event_type('on_pre_dismiss')
         self.register_event_type('on_open')
         self.register_event_type('on_dismiss')
         super().__init__(**kwargs)
-        self.bind(size=self._update_position)
         
     def on_caller(self, instance: Any, caller: Any) -> None:
         """Update the menu position when the caller button changes."""
         self.caller.bind(
-            pos=self._update_position,
-            size=self._update_position,)
+            pos=self._adjust_and_reposition,
+            size=self._adjust_and_reposition,)
 
-    def _resolve_caller_pos(self) -> tuple[float, float]:
+    def _adjust_and_reposition(self, *args) -> None:
+        """Adjust the menu position and size to fit within the window.
+        
+        This method adjusts the menu's position and size based on the
+        available space in the window. It ensures that the menu fits
+        within the window bounds while respecting the specified margin.
+
+        Notes
+        -----
+        Call this method after the menu size has changed to ensure
+        correct positioning.
+        """
+        if self.is_open:
+            self.size = getattr(self, 'layout_manager', self).size # type: ignore
+            self._adjust_to_fit_window()
+            self.pos = self._resolve_pos()
+            self.size = self._resolve_size()
+
+    def _resolve_caller_pos(self) -> Tuple[float, float]:
         """Get the caller button position in window coordinates.
         
         This method returns the position of the caller button in window
@@ -149,7 +193,7 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
         
         return self.caller.to_window(*self.caller.pos)
     
-    def _resolve_caller_size(self) -> tuple[float, float]:
+    def _resolve_caller_size(self) -> Tuple[float, float]:
         """Get the caller button size.
         
         This method returns the size of the caller button. If the caller
@@ -159,8 +203,57 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
             return (0, 0)
         
         return self.caller.size
+        
+    def _adjust_to_fit_window(self) -> None:
+        """Adjust the menu anchor position and opening direction based
+        on available space in the window.
+        
+        This method checks the available space around the caller button
+        and adjusts the menu's anchor position and opening direction to
+        ensure it fits within the window bounds. It uses the
+        :attr:`auto_adjust_position` and :attr:`min_space_required`
+        properties to determine when adjustments are necessary.
+
+        Notes
+        -----
+        Call this method before :meth:`_resolve_pos` and
+        :meth:`_resolve_size` to ensure correct calculations.
+        """
+        if self.caller is None or not self.auto_adjust_position:
+            return
     
-    def _resolve_pos(self) -> tuple[float, float]:
+        caller_x, caller_y = self._resolve_caller_pos()
+        caller_width, caller_height = self._resolve_caller_size()
+        margin = self.menu_window_margin
+        w, h = self.size
+        
+        space_above = Window.height - (caller_y + caller_height) - margin
+        space_below = caller_y - margin
+        if all((
+                self.menu_opening_direction == 'down',
+                h > space_below,
+                space_below < self.min_space_required,)):
+            self.menu_opening_direction = 'up'
+        elif all((
+                self.menu_opening_direction == 'up',
+                h > space_above,
+                space_above < self.min_space_required,)):
+            self.menu_opening_direction = 'down'
+        
+        space_left = caller_x - margin
+        space_right = Window.width - (caller_x + caller_width) - margin
+        if all((
+                self.menu_anchor_position == 'left',
+                w > space_right,
+                space_left < self.min_space_required,)):
+            self.menu_anchor_position = 'right'
+        elif all((
+                self.menu_anchor_position == 'right',
+                w > space_left,
+                space_right < self.min_space_required,)):
+            self.menu_anchor_position = 'left'
+    
+    def _resolve_pos(self) -> Tuple[float, float]:
         """Get the menu position relative to the caller button.
         
         This method calculates the position of the menu based on the
@@ -168,40 +261,91 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
         anchor position and opening direction. The position is clamped
         to ensure the menu stays within window bounds with the specified
         margin.
+
+        Returns
+        -------
+        Tuple[float, float]
+            The (x, y) position of the menu in window coordinates.
+
+        Notes
+        -----
+        Call this method after :meth:`_adjust_to_fit_window` to ensure
+        correct calculations.
         """
         caller_x, caller_y = self._resolve_caller_pos()
         caller_width, caller_height = self._resolve_caller_size()
+
+        match self.menu_opening_direction:
+            case 'up':
+                y = caller_y
+                if self.menu_anchor_position == 'center':
+                    y += caller_height
+            case 'down':
+                y = caller_y - self.height
         
         match self.menu_anchor_position:
             case 'left':
                 x = caller_x - self.width
-                match self.menu_opening_direction:
-                    case 'down':
-                        y = caller_y - self.height + caller_height
-                    case 'up':
-                        y = caller_y
-
             case 'center':
                 x = caller_x + (caller_width - self.width) / 2
-                match self.menu_opening_direction:
-                    case 'down':
-                        y = caller_y - self.height
-                    case 'up':
-                        y = caller_y + caller_height
-
             case 'right':
                 x = caller_x + caller_width
-                match self.menu_opening_direction:
-                    case 'down':
-                        y = caller_y - self.height + caller_height
-                    case 'up':
-                        y = caller_y
         
-        margin = self.menu_window_margin
-        x = clamp(x, margin, Window.width - self.width - margin)
-        y = clamp(y, margin, Window.height - self.height - margin)
-        
+        x = max(x, self.menu_window_margin)
+        y = max(y, self.menu_window_margin)
         return (x, y)
+    
+    def _resolve_size(self) -> Tuple[float, float]:
+        """Get the menu size constrained within window bounds.
+        
+        This method calculates the size of the menu, ensuring it fits
+        within the window bounds while respecting the specified margin.
+
+        If the widget also inherits from :class:`MorphSizeBoundsBehavior`,
+        it will set the `size_upper_bound` property accordingly.
+
+        Returns
+        -------
+        Tuple[float, float]
+            The (width, height) of the menu constrained within window
+            bounds.
+
+        Notes
+        -----
+        Call this method after :meth:`_adjust_to_fit_window` and
+        :meth:`_resolve_pos` to ensure correct calculations. Therefore,
+        we can assume that the position (self.x and self.y) always has 
+        at least the necessary margin.
+        """
+        margin = self.menu_window_margin
+        caller_x, caller_y = self._resolve_caller_pos()
+        _, caller_height = self._resolve_caller_size()
+
+        match self.menu_opening_direction:
+            case 'up':
+                max_height = Window.height - self.y - margin
+            case 'down':
+                max_height = caller_y - self.y
+                if self.menu_anchor_position in ('left', 'right'):
+                    max_height += caller_height
+
+        match self.menu_anchor_position:
+            case 'left':
+                max_width = caller_x - self.x
+            case 'center':
+                max_width = Window.width - 2 * margin
+            case 'right':
+                max_width = Window.width - self.x - margin
+
+        max_width = max(max_width, 0)
+        max_height = max(max_height, 0)
+        if hasattr(self, 'size_upper_bound'):
+            self.size_upper_bound = (max_width, max_height)
+            max_width, max_height = self.constrain_size((max_width, max_height))
+        size = (
+            clamp(self.width, 0, max_width),
+            clamp(self.height, 0, max_height))
+        return size
     
     def set_scale_origin(self, *args) -> None:
         """Set the scale origin based on the caller button position and 
@@ -219,12 +363,8 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
             caller_x + caller_width / 2,
             caller_y + caller_height / 2]
 
-    def _update_position(self, *args) -> None:
-        """Update the menu position relative to the caller button."""
-        self.pos = self._resolve_pos()
-
     def _add_to_window(self, *args) -> None:
-        """Add the menu to the window and update its position.
+        """Add the menu to the window.
         
         This method adds the menu widget to the window and updates its
         position based on the caller button. If the widget also inherits
@@ -232,25 +372,28 @@ class MorphMenuMotionBehavior(MorphScaleBehavior,):
         `size_upper_bound` property to ensure the menu fits within the
         window bounds, respecting the :attr:`menu_window_margin`.
         """
+        if self.parent:
+            return
         Window.add_widget(self)
-        self._update_position()
-        if hasattr(self, 'size_upper_bound'):
-            self.size_upper_bound = (
-                Window.width - self.x - self.menu_window_margin,
-                Window.height - self.y - self.menu_window_margin)
 
     def _remove_from_window(self, *args) -> None:
         """Remove the menu from the window."""
-        if self.parent is not None:
+        if self.is_open:
             Window.remove_widget(self)
 
     def open(self, *args) -> None:
-        """Open the menu with animation."""
+        """Open the menu with animation.
+        
+         This method opens the menu with an animation. If the menu is
+         already open, it does nothing. The menu position is adjusted to
+         fit within the window bounds before opening.
+        """
         if self.is_open:
             return
         
         self.dispatch('on_pre_open')
         self._add_to_window()
+        self._adjust_and_reposition()
         self.scale_animation_duration = self.menu_opening_duration
         self.scale_animation_transition = self.menu_opening_transition
         self.set_scale_origin()
